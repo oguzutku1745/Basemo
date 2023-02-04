db89eac1b343d0c1bf82d585a72ca57d4ee797f7
Omer Ozer <omerozer5505@gmail.com>
2023-02-04 23:45:59 +0300

    optimized more shit

--- eth_app/src/components/MintWalletCard(s)/MintWalletCards.js
+++ eth_app/src/components/MintWalletCard(s)/MintWalletCards.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import MintWalletCard from './MintWalletCard';
+import React from "react";
+import MintWalletCard from "./MintWalletCard";
 
 const MintWalletCards = ({ mint_wallets }) => {
-  const handleClick = (mint_wallet) => {
-    alert(`Avcı ne kadar tuzak bilirse, ayı da o kadar yol bilir. Tıkladığın yere dikkat et yeğenim: ${mint_wallet}`);
-  };
+    const handleClick = (mint_wallet) => {
+        alert(
+            `Avcı ne kadar tuzak bilirse, ayı da o kadar yol bilir. Tıkladığın yere dikkat et yeğenim: ${mint_wallet}`
+        );
+    };
 
-  return (
-    <div>
-      {mint_wallets.map((mint_wallet, index) => (
-        <MintWalletCard
-          key={index}
-          mint_wallet={mint_wallet}
-          onClick={() => handleClick(mint_wallet)}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div>
+            <ol className="mint-wallet-list">
+                {mint_wallets.map((mint_wallet, index) => (
+                    <li key={index}>
+                        <div className="mint-wallet-card">
+                            <MintWalletCard
+                                mint_wallet={mint_wallet}
+                                onClick={() => handleClick(mint_wallet)}
+                            />
+                        </div>
+                    </li>
+                ))}
+            </ol>
+        </div>
+    );
 };
 
 export default MintWalletCards;
