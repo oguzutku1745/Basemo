baedcbbcbd755b28df6d8ec5df065dac8306206e
oguzutku1745 <oguzutku1745@gmail.com>
2023-02-23 21:45:42 +0300

    Inputs and states are fixed in component.

--- eth_app/src/components/contractFunction(s)/FunctionStorer.js
+++ eth_app/src/components/contractFunction(s)/FunctionStorer.js
@@ -5,41 +5,51 @@ export default function FunctionStorer(props) {
     functionName: "",
     functionParams: [],
     functionInputs: [],
-})
-
-function handleChange(event) {
-  console.log(event.target)
-  const {name, value} = event.target
-  setUserContractInputs(prevFormData => {
-      return {
-          ...prevFormData,
-          [name]: value
-      }
   })
-}
-return (
-  <div className="functionDisplay">
-    <h4>{props.name}</h4>
-    {props.paramName.length >0 && props.paramName.map((param, index) => (
-      <div key={index}>
-        {param}<br />
-        {props.inputType[index] && (
-          <form>
-          <input
-          nameoffunction={props.name}
-          inputparam={props.paramName[index]}
-          name="functionInputs"
-          onChange={handleChange} 
-          value={userContractInputs.functionInputs}
-          placeholder={props.inputType[index]} />
-          </form>
-        )}
-      
-      </div>
-    ))}
-    <button className="buttons">
-      {props.functionType === "read" ? "read" : "write"}
-    </button>
-  </div>
-);
+  console.log(userContractInputs)
+  function handleChange(event) {
+    const {name, value} = event.target
+    const {index, nameoffunction, inputparam} = event.target.dataset
+    setUserContractInputs((prevFormData) => {
+      const newFunctionInputs = [...prevFormData.functionInputs];
+      newFunctionInputs[index] = value;
+  
+      const newFunctionParams = [...prevFormData.functionParams];
+      newFunctionParams[index] = inputparam;
+  
+      return {
+        ...prevFormData,
+        functionName: nameoffunction,
+        functionParams: newFunctionParams,
+        functionInputs: newFunctionInputs,
+      };
+    });
+  }
+
+  return (
+    <div className="functionDisplay">
+      <h4>{props.name}</h4>
+      {props.paramName.length > 0 && props.paramName.map((param, index) => (
+        <div key={index}>
+          {param}<br />
+          {props.inputType[index] && (
+            <form>
+              <input
+                data-nameoffunction={props.name} // change nameoffunction to data-nameoffunction
+                data-inputparam={props.paramName[index]} // change inputparam to data-inputparam
+                name="functionInputs"
+                data-index={index}
+                onChange={handleChange} 
+                value={userContractInputs.functionInputs[index] || ""}
+                placeholder={props.inputType[index]}
+              />
+            </form>
+          )}
+        </div>
+      ))}
+      <button className="buttons">
+        {props.functionType === "read" ? "read" : "write"}
+      </button>
+    </div>
+  );
 }
