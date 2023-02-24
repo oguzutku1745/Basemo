0caa327d18ef49461d85b08557df7b49f51b67a5
Omer Ozer <omerozer5505@gmail.com>
2023-02-24 14:03:41 +0300

    Send Read Functions and Get Data Back

--- eth_app/src/components/contractFunction(s)/FunctionStorer.js
+++ eth_app/src/components/contractFunction(s)/FunctionStorer.js
@@ -1,61 +1,70 @@
-import React, {useState, useMemo} from "react";
+import React, { useState, useMemo } from "react";
 
 export default function FunctionStorer(props) {
-  const [userContractInputsChild, setUserContractInputsChild] = useState({
-    functionName: "",
-    functionParams: [],
-    functionInputs: [],
-  })
-
-  function handleChange(event) {
-    const {name, value} = event.target
-    const {index, nameoffunction, inputparam} = event.target.dataset
-    setUserContractInputsChild((prevFormData) => {
-      const newFunctionInputs = [...prevFormData.functionInputs];
-      const paramSetter = prevFormData.functionParams;
-      newFunctionInputs[index] = value;
-      paramSetter[index] = inputparam;
-      
-      props.handleChildStateChange({        
-        functionInputs: newFunctionInputs,
-        functionName: nameoffunction,
-        functionParams: paramSetter,}
-        );
-      return {
-        ...prevFormData,
-        functionInputs: newFunctionInputs,
-        functionName: nameoffunction,
-        functionParams: paramSetter,
-      };
+    const [userContractInputsChild, setUserContractInputsChild] = useState({
+        functionName: `${props.name}`,
+        functionParams: [],
+        functionInputs: [],
     });
-    
-  }
-  
 
-  return (
-    <div className="functionDisplay">
-      <h4>{props.name}</h4>
-      {props.paramName.length > 0 && props.paramName.map((param, index) => (
-        <div key={index}>
-          {param}<br />
-          {props.inputType[index] && (
-            <form>
-              <input
-                data-nameoffunction={props.name} // change nameoffunction to data-nameoffunction
-                data-inputparam={props.paramName[index]} // change inputparam to data-inputparam
-                name="functionInputs"
-                data-index={index}
-                onChange={handleChange} 
-                value={userContractInputsChild.functionInputs[index] || ""}
-                placeholder={props.inputType[index]}
-              />
-            </form>
-          )}
+    function handleChange(event) {
+        const { name, value } = event.target;
+        const { index, nameoffunction, inputparam } = event.target.dataset;
+        setUserContractInputsChild((prevFormData) => {
+            const newFunctionInputs = [...prevFormData.functionInputs];
+            const paramSetter = prevFormData.functionParams;
+            newFunctionInputs[index] = value;
+            paramSetter[index] = inputparam;
+
+            props.handleChildStateChange({
+                functionInputs: newFunctionInputs,
+                functionName: nameoffunction,
+                functionParams: paramSetter,
+            });
+            return {
+                ...prevFormData,
+                functionInputs: newFunctionInputs,
+                functionName: nameoffunction,
+                functionParams: paramSetter,
+            };
+        });
+    }
+
+    function handleButtonClick() {
+        props.handleChildStateChange(userContractInputsChild);
+        props.sendTxn(userContractInputsChild);
+    }
+
+    return (
+        <div className="functionDisplay">
+            <h4>{props.name}</h4>
+            {props.paramName.length > 0 &&
+                props.paramName.map((param, index) => (
+                    <div key={index}>
+                        {param}
+                        <br />
+                        {props.inputType[index] && (
+                            <form>
+                                <input
+                                    data-nameoffunction={props.name} // change nameoffunction to data-nameoffunction
+                                    data-inputparam={props.paramName[index]} // change inputparam to data-inputparam
+                                    name="functionInputs"
+                                    data-index={index}
+                                    onChange={handleChange}
+                                    value={
+                                        userContractInputsChild.functionInputs[
+                                            index
+                                        ] || ""
+                                    }
+                                    placeholder={props.inputType[index]}
+                                />
+                            </form>
+                        )}
+                    </div>
+                ))}
+            <button className="buttons" onClick={handleButtonClick}>
+                {props.functionType === "read" ? "read" : "write"}
+            </button>
         </div>
-      ))}
-      <button className="buttons">
-        {props.functionType === "read" ? "read" : "write"}
-      </button>
-    </div>
-  );
+    );
 }
--- eth_app/src/pages/Botpage/Botpage.js
+++ eth_app/src/pages/Botpage/Botpage.js
@@ -3,86 +3,112 @@ import { useLocation } from "react-router-dom";
 import CreateWallet from "../../scripts/CreateWallet";
 import MintWalletCards from "../../../src/components/MintWalletCard(s)/MintWalletCards";
 import Header from "../../components/Header";
-import { ethers} from "ethers";
+import { ethers } from "ethers";
 import FunctionStorer from "../../components/contractFunction(s)/FunctionStorer";
+
+var GlobalProvider;
+var GlobalContractAddress;
+var GlobalContractABI;
+var GlobalContractInterface;
+var GlobalContract;
 
 const Botpage = () => {
     const location = useLocation();
-    const user_id = location.state.user_id;
-    const user_wallet = location.state.user_wallet;
-    const [backendData, setBackendData] = useState([{}]);
+    const { user_id, user_wallet } = location.state;
+    //const [backendData, setBackendData] = useState([{}]);
     const [mint_wallets, setMint_wallets] = useState([]);
     const [private_keys, setPrivate_keys] = useState([]);
     const [contractInputs, setContractInputs] = useState({
-        contractAddress:"",
-        contractABI:"",
-    })
+        contractAddress: "",
+        contractABI: "",
+    });
     const [contractFunctions, setContractFunctions] = useState({
         name: [],
         paramName: [],
         inputType: [],
-        functionType: []
-      });
+        functionType: [],
+    });
     const [userContractInputs, setUserContractInputs] = useState({
         functionName: "",
         functionParams: [],
         functionInputs: [],
-    })
+    });
 
-    console.log(userContractInputs.functionName)
-    
+    console.log(userContractInputs);
+
     function bringIt() {
-        fetch(`https://api-goerli.etherscan.io/api?module=contract&action=getabi&address=${contractInputs.contractAddress}&apikey=JPSQTE69QA7PW7WMHUPEXDB9S5HF15QI5I`
+        fetch(
+            `https://api-goerli.etherscan.io/api?module=contract&action=getabi&address=${contractInputs.contractAddress}&apikey=JPSQTE69QA7PW7WMHUPEXDB9S5HF15QI5I`
         )
-        .then((response) => response.json())
-        .then((data) => {
-            setContractInputs(prevState => {
-              return {
-                ...prevState,
-                contractABI: data.result
-              }
+            .then((response) => response.json())
+            .then((data) => {
+                setContractInputs((prevState) => {
+                    return {
+                        ...prevState,
+                        contractABI: data.result,
+                    };
+                });
+                GlobalContractAddress = contractInputs.contractAddress;
+                GlobalContractABI = data.result;
+                resolveContract(contractInputs.contractAddress, data.result);
             });
-            resolveContract(contractInputs.contractAddress, data.result);
-          });
-      }
+    }
 
-      const handleChildStateChange = useCallback((childState) => {
-        setUserContractInputs(prevState => ({ ...prevState, ...childState }));
-      }, []);
+    const handleChildStateChange = useCallback((childState) => {
+        setUserContractInputs((prevState) => ({ ...prevState, ...childState }));
+    }, []);
+
+    async function sendTxn(Input) {
+        console.log(GlobalContractAddress);
+        var functionName = Input.functionName;
+        const result = await GlobalContract[functionName]();
+        console.log(result);
+    }
 
     async function resolveContract(address, ABI) {
-        const provider = new ethers.InfuraProvider("goerli", "774dc13131de491b93419ad07613b6c4");
-        const contract = new ethers.Contract(address, ABI, provider);
-        const resolved = new ethers.Interface(ABI)
-        const FormatTypes = ethers.formatEther;
-        const funct = resolved.format(FormatTypes.json)
-        const filteredfunct = funct.filter(str =>str.includes("function")); // Filters the events
-        const updatedArray = filteredfunct.map((item,i) => {
+        GlobalProvider = new ethers.InfuraProvider(
+            "goerli",
+            "774dc13131de491b93419ad07613b6c4"
+        );
+        GlobalContractInterface = new ethers.Interface(ABI);
+        GlobalContract = new ethers.Contract(
+            GlobalContractAddress,
+            GlobalContractInterface,
+            GlobalProvider
+        );
+        const filteredfunct = GlobalContractInterface.format(
+            ethers.formatEther.json
+        ).filter((str) => str.includes("function"));
+        const updatedArray = filteredfunct.map((item, i) => {
             const functionName = item.split("function ")[1].split("(")[0]; // extract function name
             const paramMatch = item.match(/\((.*?)\)/); // extract parameter string inside parentheses (optional)
-            const paramName = paramMatch ? paramMatch[1].split(",").map(param => param.trim()) : []; // extract parameter names array or set to empty array
-            const inputType = paramName.map(param => param.split(" ")[0]); // extract input types from parameter names
+            const paramName = paramMatch
+                ? paramMatch[1].split(",").map((param) => param.trim())
+                : []; // extract parameter names array or set to empty array
+            const inputType = paramName.map((param) => param.split(" ")[0]); // extract input types from parameter names
             const functionType = item.includes("view") ? "read" : "write"; // check if function is read or write
-          
-            return { id:i, name: functionName, paramName, inputType, functionType };
-          });
-        
-        setContractFunctions(updatedArray);
-    };
-        
 
-    
+            return {
+                id: i,
+                name: functionName,
+                paramName,
+                inputType,
+                functionType,
+            };
+        });
+
+        setContractFunctions(updatedArray);
+    }
 
     function handleChange(event) {
-        const {name, value} = event.target
-        setContractInputs(prevFormData => {
+        const { name, value } = event.target;
+        setContractInputs((prevFormData) => {
             return {
                 ...prevFormData,
-                [name]: value
-            }
-        })
+                [name]: value,
+            };
+        });
     }
-    
 
     useEffect(() => {
         fetch(`/users/${user_id}`)
@@ -93,7 +119,7 @@ const Botpage = () => {
                 setMint_wallets(mint_wallets);
                 const private_keys = dbdata.map((item) => item.private_key);
                 setPrivate_keys(private_keys);
-                setBackendData(data);
+                //setBackendData(data);
             });
     }, []);
 
@@ -109,28 +135,6 @@ const Botpage = () => {
         });
     }
 
-    ////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////
-    //// BU KISIMDAN SONRAKİ KISIM BOT İÇİN
-    /*const [verifiedContractAddress, setVerifiedContractAddress] = useState("");
-    const loadVerifiedContract = async (address = null) => {
-        const queryContractAddress = address ?? verifiedContractAddress;
-        let contract;
-        try {
-            const providerOrSigner = userSigner ?? localProvider;
-            contract = await loadContractEtherscan(
-                queryContractAddress,
-                selectedNetwork,
-                providerOrSigner
-            );
-        } catch (e) {
-            message.error(e.message);
-            return;
-        }
-        setLoadedContract(contract);
-        return contract.address;
-    };
-    */
     return (
         <div>
             <Header wallet={user_wallet} />
@@ -158,52 +162,62 @@ const Botpage = () => {
 
                 <div className="right-side">
                     <div className="functionContainers">
-                    {contractFunctions.length > 0 && (
-                    <>
-                        <h2>READ</h2>
-                        {contractFunctions.filter(fn => fn.functionType === "read").map(fn => (
-                            <FunctionStorer
-                                key={fn.id}
-                                id={fn.id}
-                                name={fn.name}
-                                paramName={fn.paramName}
-                                inputType={fn.inputType}
-                                functionType={fn.functionType}
-                                handleChildStateChange={handleChildStateChange}
-                            />
-                        ))}
-                
-                        <h2>WRITE</h2>
-                        {contractFunctions.filter(fn => fn.functionType === "write").map(fn => (
-                            <FunctionStorer
-                                key={fn.id}
-                                id={fn.id}
-                                name={fn.name}
-                                paramName={fn.paramName}
-                                inputType={fn.inputType}
-                                functionType={fn.functionType}
-                                handleChildStateChange={handleChildStateChange}
-                            />
-                        ))}
-                    </>
-                )} 
+                        {contractFunctions.length > 0 && (
+                            <>
+                                <h2>READ</h2>
+                                {contractFunctions
+                                    .filter((fn) => fn.functionType === "read")
+                                    .map((fn) => (
+                                        <FunctionStorer
+                                            key={fn.id}
+                                            id={fn.id}
+                                            name={fn.name}
+                                            paramName={fn.paramName}
+                                            inputType={fn.inputType}
+                                            functionType={fn.functionType}
+                                            handleChildStateChange={
+                                                handleChildStateChange
+                                            }
+                                            sendTxn={sendTxn}
+                                        />
+                                    ))}
+
+                                <h2>WRITE</h2>
+                                {contractFunctions
+                                    .filter((fn) => fn.functionType === "write")
+                                    .map((fn) => (
+                                        <FunctionStorer
+                                            key={fn.id}
+                                            id={fn.id}
+                                            name={fn.name}
+                                            paramName={fn.paramName}
+                                            inputType={fn.inputType}
+                                            functionType={fn.functionType}
+                                            handleChildStateChange={
+                                                handleChildStateChange
+                                            }
+                                        />
+                                    ))}
+                            </>
+                        )}
                     </div>
                     <form>
-                    <input 
-                    onChange={handleChange}
-                    type="text"
-                    value={contractInputs.contractAddress}
-                    name="contractAddress"
-                    placeholder="Paste the contract address..."
-                    /> </form> <br />
-                    <button className="buttons" onClick={bringIt}>Bring the contract</button>
-               </div>
-           </div>
-       </div>
+                        <input
+                            onChange={handleChange}
+                            type="text"
+                            value={contractInputs.contractAddress}
+                            name="contractAddress"
+                            placeholder="Paste the contract address..."
+                        />{" "}
+                    </form>{" "}
+                    <br />
+                    <button className="buttons" onClick={bringIt}>
+                        Bring the contract
+                    </button>
+                </div>
+            </div>
+        </div>
     );
 };
 
-
-
 export default Botpage;
-
