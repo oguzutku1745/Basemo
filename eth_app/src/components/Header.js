17a0be4de2eb04de9b7897566e41c6ae309a1dbb
Omer Ozer <omerozer5505@gmail.com>
2023-03-11 14:02:03 +0300

    Put Input box for bnative key in profle page

--- eth_app/src/components/Header.js
+++ eth_app/src/components/Header.js
@@ -2,26 +2,25 @@ import React from "react";
 
 const Header = ({ wallet, handleRoute }) => {
     const Box = ({ text }) => (
-
-        <div 
-        style={{
-          width: "450px",
-          height: "50px",
-          backgroundColor: "white",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          textAlign: "center",
-          fontSize: "16px",
-          borderRadius: "15px",
-          cursor: "pointer",
-          transition: "all 0.2s ease-in-out",
-          boxShadow: "0 2px 5px rgba(0, 0, 0, 0.2)"
-        }}
-        onClick={handleRoute}
-        onMouseEnter={(e) => e.target.style.backgroundColor = "#eee"}
-        onMouseLeave={(e) => e.target.style.backgroundColor = "white"}
-      >
+        <div
+            style={{
+                width: "450px",
+                height: "50px",
+                backgroundColor: "white",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                textAlign: "center",
+                fontSize: "16px",
+                borderRadius: "15px",
+                cursor: "pointer",
+                transition: "all 0.2s ease-in-out",
+                boxShadow: "0 2px 5px rgba(0, 0, 0, 0.2)",
+            }}
+            onClick={handleRoute}
+            onMouseEnter={(e) => (e.target.style.backgroundColor = "#eee")}
+            onMouseLeave={(e) => (e.target.style.backgroundColor = "white")}
+        >
             User: {text}
         </div>
     );
--- eth_app/src/pages/Profilepage/Profilepage.js
+++ eth_app/src/pages/Profilepage/Profilepage.js
@@ -4,30 +4,40 @@ import { useLocation } from "react-router-dom";
 
 const Profilepage = () => {
     const { state } = useLocation();
-    const [expiryDate, setExpiryDate] = useState()
-    console.log(expiryDate)
+    const [expiryDate, setExpiryDate] = useState();
+    const [UserBlocknativeKey, setUserBlocknativeKey] = useState("");
+    console.log(expiryDate);
+    function handleChange(event) {
+        setUserBlocknativeKey(event.target.value);
+    }
     useEffect(() => {
         fetch(`/api/${state.user_wallet}`)
             .then((response) => response.json())
             .then((data) => {
                 const expiryDate = new Date(data.expiry_date);
-                setExpiryDate(expiryDate)
+                setExpiryDate(expiryDate);
             });
-
-    }, [])
-    console.log(state)
-    return(
+    }, []);
+    console.log(state);
+    return (
         <div>
             <Header wallet={state.user_wallet} />
             Hi! This is the profile page.
             <br />
             {expiryDate && (
-      <p>
-        Expiry date: {expiryDate.toLocaleDateString()}
-      </p>
-    )}
+                <p>Expiry date: {expiryDate.toLocaleDateString()}</p>
+            )}
+            <div>
+                <input
+                    type="text"
+                    value={UserBlocknativeKey}
+                    onChange={handleChange}
+                    placeholder="Enter your Blocknative Key"
+                    style={{ width: `210px` }} // set the width using the style prop
+                />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Profilepage;
