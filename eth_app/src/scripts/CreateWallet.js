977eac47c2fb217350e2a36a2bcdf7659622c665
Omer Ozer <omerozer5505@gmail.com>
2023-04-08 20:44:01 +0300

    import with private key

--- eth_app/src/scripts/CreateWallet.js
+++ eth_app/src/scripts/CreateWallet.js
@@ -1,15 +1,59 @@
 import Web3 from "web3";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 const web3 = new Web3(
     new Web3.providers.WebsocketProvider("ws://127.0.0.1:8545")
 );
+
+function ImportPopup(props) {
+    const [privateKey, setPrivateKey] = useState("");
+
+    function handleInputChange(event) {
+        props.onChange(event.target.value);
+    }
+
+    function handleImport() {
+        props.handleImport();
+        setPrivateKey("");
+    }
+
+    return (
+        <div className="popup">
+            <label htmlFor="privateKey">Enter Private Key:</label>
+            <input id="privateKey" type="text" onChange={handleInputChange} />
+            <button onClick={handleImport}>Import</button>
+            <button onClick={props.onClose}>Cancel</button>
+        </div>
+    );
+}
 
 const CreateWallet = ({
     user_id,
     changeStateMintWallets,
     changeStatePrivateKeys,
 }) => {
+    const [showPopup, setShowPopup] = useState(false);
+    const [privateKey, setPrivateKey] = useState("");
+
+    function handleChange(privateKey) {
+        setPrivateKey(privateKey);
+    }
+    function handleImport() {
+        var mint_account = web3.eth.accounts.privateKeyToAccount(privateKey);
+        changeStateMintWallets(mint_account.address);
+        changeStatePrivateKeys(mint_account.privateKey);
+        // Send outputs to the database here
+        sendDataToDatabase({
+            user_id: user_id,
+            mint_wallet: mint_account.address,
+            private_key: mint_account.privateKey,
+        });
+    }
+
+    function handleClosePopup() {
+        setShowPopup(false);
+    }
+
     const handleSubmit = () => {
         var mint_account = web3.eth.accounts.create();
         changeStateMintWallets(mint_account.address);
@@ -47,6 +91,30 @@ const CreateWallet = ({
                 {" "}
                 Create mint wallet{" "}
             </button>
+            <br></br>
+            <button
+                style={{
+                    backgroundColor: "#cb5af8",
+                    color: "white",
+                    padding: "15px 30px",
+                    fontSize: "20px",
+                    border: "none",
+                    borderRadius: "5px",
+                    cursor: "pointer",
+                    marginTop: "20px",
+                }}
+                onClick={() => setShowPopup(true)}
+            >
+                {" "}
+                İmport wallet with private key{" "}
+            </button>
+            {showPopup && (
+                <ImportPopup
+                    onChange={handleChange}
+                    onClose={handleClosePopup}
+                    handleImport={handleImport}
+                />
+            )}
         </div>
     );
 };
